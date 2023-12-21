7411d93c3ea2d82593bf0f88565bfa7e62dbca63
echanovevaro <echanovevaro@gmail.com>
2023-12-21 15:32:35 +0100

    fixes sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,29 @@
-import "./index.css"
-import BooksDashboard from "./components/BooksDashboard"
-import BookDetail from "./components/BookDetail"
-import BooksList from "./components/BooksList"
-import { useUserBooks } from "./store/userBooks"
-import "bootstrap/dist/css/bootstrap.min.css"
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
-import Modal from "react-bootstrap/Modal"
+import "./index.css";
+import BooksDashboard from "./components/BooksDashboard";
+import BookDetail from "./components/BookDetail";
+import BooksList from "./components/BooksList";
+import { useUserBooks } from "./store/userBooks";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Modal from "react-bootstrap/Modal";
 
 function App() {
   const { book, clearBook } = useUserBooks((state) => ({
     book: state.book,
     clearBook: state.clearBook,
-  }))
+  }));
 
   return (
     <>
-      <Modal
-        size="lg"
-        centered
-        show={!!book.title}
-        onHide={() => clearBook()}
-      >
+      <Modal size="lg" centered show={!!book.title} onHide={() => clearBook()}>
         <Modal.Header closeButton></Modal.Header>
         <BookDetail />
       </Modal>
-      <Container
-        fluid
-        className="p-0 m-0"
-      >
+      <Container fluid className="p-0 m-0">
         <Row>
-          <Col
-            xs={12}
-            className="p-4 grid-container"
-          >
+          <Col xs={12} className="p-5 pt-0 grid-container">
             <BooksDashboard book={book} />
           </Col>
           {/* <Col
@@ -46,16 +35,13 @@ function App() {
         </Col> */}
         </Row>
         <Row className="b-0">
-          <Col
-            xs={12}
-            className="p-4 lists-container"
-          >
+          <Col xs={12} className="p-5 lists-container">
             <BooksList />
           </Col>
         </Row>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
